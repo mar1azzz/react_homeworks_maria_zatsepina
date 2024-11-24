7a36cba323e016f06e9840fcaea2730127220ff2
Mary Zatsepina <zatsepina.maria@gmail.com>
2024-11-24 13:25:06 +0300

    feat: rewritten App.jsx to functional approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import MenuSection from './components/MenuSection/MenuSection';
 
-class App extends Component {
-    state = {
-        cartCount: 0
+const App = () => {
+    const [cartCount, setCartCount] = useState(0);
+    const addToCart = (quantity) => {
+        setCartCount((prevCount) => prevCount + quantity);
     };
-
-    addToCart = (quantity) => {
-        this.setState((prevState) => ({
-            cartCount: prevState.cartCount + quantity
-        }));
-    };
-
-    render() {
-        return (
-            <>
-                <Header cartCount={this.state.cartCount} />
-                <MenuSection addToCart={this.addToCart} />
-                <Footer />
-            </>
-        );
-    }
-}
-
+    return (
+        <>
+            <Header cartCount={cartCount} />
+            <MenuSection addToCart={addToCart} />
+            <Footer />
+        </>
+    );
+};
 export default App;
